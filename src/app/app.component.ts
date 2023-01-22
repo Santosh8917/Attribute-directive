7819a3cf55511bb8614ca26fa982a9abb2f41ce4
Santosh Behera01 <Santosh.Behera01@godigit.com>
2023-01-22 12:51:20 +0530

    atrribute directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'attribute';
+//ngStyle
+  myStyles = {
+    width: '150px',
+    height: '100px',
+    background:'red',
+    color:'yellow',
+    
+  }
+
+  addStyles(){
+    this.myStyles['color'] = 'white';
+    this.myStyles['background'] = 'blue'
+  }
+
+  //ngClass
+myClasses = {
+  box: true,
+  border: false,
+  circle: false
+};
+
+changeShape(){
+  // this.myClasses.border = true;
+ // this.myClasses.circle = true;
+
+ //dynamically changes shape from rectangular to circle and circle to rectangular
+ this.myClasses.border = !this.myClasses.border;
+ this.myClasses.circle =!this.myClasses.circle
+
+}
+
 }
